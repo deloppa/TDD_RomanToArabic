8b99ca122fb47f20242e17b22063e24d1a79e74d
deloppa <97598343+deloppa@users.noreply.github.com>
2024-02-24 00:59:13 +0200

    tested argument exceptions and main values

--- RomanToArabic/Converter.cs
+++ RomanToArabic/Converter.cs
@@ -1,21 +1,47 @@
-﻿namespace RomanToArabic;
+﻿using System.Text.RegularExpressions;
+
+namespace RomanToArabic;
 
 public static class Converter
 {
-    private static Dictionary<int, string> _values = new Dictionary<int, string>()
+    private static Dictionary<string, int> _values = new Dictionary<string, int>()
     {
-        { 1, "I" },
-        { 4, "IV" },
-        { 5, "V" },
-        { 9, "IX" },
-        { 10, "X" },
-        { 40, "XL" },
-        { 50, "L" },
-        { 90, "XC" },
-        { 100, "C" },
-        { 400, "CD" },
-        { 500, "D" },
-        { 900, "CM" },
-        { 1000, "M" }
+        { "I", 1 },
+        { "IV", 4 },
+        { "V", 5 },
+        { "IX", 9 },
+        { "X", 10 },
+        { "XL", 40 },
+        { "L", 50 },
+        { "XC", 90 },
+        { "C", 100 },
+        { "CD", 400 },
+        { "D", 500 },
+        { "CM", 900 },
+        { "M", 1000 }
     };
+
+    public static int ConvertRomanToArabic(string roman)
+    {
+        roman = roman.ToUpper();
+
+        if (roman.Equals("")) throw new ArgumentException("String cannot be empty");
+
+        if(!IsValid(roman)) throw new ArgumentException("Write valid roman number between 1 and 3999");
+
+        if (_values.ContainsKey(roman)) return _values[roman];
+
+        int result = 0;
+
+
+        return result;
+    }
+
+    private static bool IsValid(string roman)
+    {
+        string pattern = @"^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$";
+        Regex regex = new Regex(pattern);
+
+        return regex.IsMatch(roman);
+    }
 }
---
+++ RomanToArabicTests/ConverterTests.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanToArabic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToArabic.Tests
+{
+    [TestClass()]
+    public class ConverterTests
+    {
+        [TestMethod()]
+        public void WhenEmpty_ThenArgumentException()
+        {
+            Exception exeption = Assert.ThrowsException<ArgumentException>(() => 
+            {
+                 Converter.ConvertRomanToArabic("");
+            });
+
+            Assert.AreEqual("String cannot be empty", exeption.Message);
+        }
+
+        [TestMethod()]
+        public void WhenVII_I_ThenArgumentException()
+        {
+            Exception exeption = Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Converter.ConvertRomanToArabic("VII_I");
+            });
+
+            Assert.AreEqual("Write valid roman number between 1 and 3999", exeption.Message);
+        }
+
+        [TestMethod()]
+        public void When1345_ThenArgumentException()
+        {
+            Exception exeption = Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Converter.ConvertRomanToArabic("1345");
+            });
+
+            Assert.AreEqual("Write valid roman number between 1 and 3999", exeption.Message);
+        }
+
+        [TestMethod()]
+        public void WhenMMMM_ThenArgumentException()
+        {
+            Exception exeption = Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Converter.ConvertRomanToArabic("MMMM");
+            });
+
+            Assert.AreEqual("Write valid roman number between 1 and 3999", exeption.Message);
+        }
+
+        [TestMethod]
+        public void WhenI_Then1()
+        {
+            Assert.AreEqual(1, Converter.ConvertRomanToArabic("I"));
+        }
+
+        [TestMethod]
+        public void WhenIV_Then4()
+        {
+            Assert.AreEqual(4, Converter.ConvertRomanToArabic("IV"));
+        }
+
+        [TestMethod]
+        public void WhenV_Then5()
+        {
+            Assert.AreEqual(5, Converter.ConvertRomanToArabic("V"));
+        }
+
+        [TestMethod]
+        public void WhenIX_Then9()
+        {
+            Assert.AreEqual(9, Converter.ConvertRomanToArabic("IX"));
+        }
+
+        [TestMethod]
+        public void WhenX_Then10()
+        {
+            Assert.AreEqual(10, Converter.ConvertRomanToArabic("X"));
+        }
+
+        [TestMethod]
+        public void WhenXL_Then40()
+        {
+            Assert.AreEqual(40, Converter.ConvertRomanToArabic("XL"));
+        }
+
+        [TestMethod]
+        public void WhenL_Then50()
+        {
+            Assert.AreEqual(50, Converter.ConvertRomanToArabic("L"));
+        }
+
+        [TestMethod]
+        public void WhenXC_Then90()
+        {
+            Assert.AreEqual(90, Converter.ConvertRomanToArabic("XC"));
+        }
+
+        [TestMethod]
+        public void WhenC_Then100()
+        {
+            Assert.AreEqual(100, Converter.ConvertRomanToArabic("C"));
+        }
+
+        [TestMethod]
+        public void WhenCD_Then400()
+        {
+            Assert.AreEqual(400, Converter.ConvertRomanToArabic("CD"));
+        }
+
+        [TestMethod]
+        public void WhenD_Then500()
+        {
+            Assert.AreEqual(500, Converter.ConvertRomanToArabic("D"));
+        }
+
+        [TestMethod]
+        public void WhenCM_Then900()
+        {
+            Assert.AreEqual(900, Converter.ConvertRomanToArabic("CM"));
+        }
+
+        [TestMethod]
+        public void WhenM_Then1000()
+        {
+            Assert.AreEqual(1000, Converter.ConvertRomanToArabic("M"));
+        }
+    }
+}
